d580bf45ffd5937c6301480bd6d63810e5149efd
Simon Wendel <mail@simonwendel.se>
2018-06-17 21:23:39 +0200

    fact: bunch up two Array.map calls by composition

--- SokobanFS.Game/Parse.fs
+++ SokobanFS.Game/Parse.fs
@@ -62,8 +62,8 @@ module Parse =
 
         rows
         |> Array.ofSeq
-        |> Array.map trimEndsOffEachRow
-        |> Array.map (  Array.ofSeq 
+        |> Array.map (  trimEndsOffEachRow
+                     >> Array.ofSeq 
                      >> Array.map toTile 
                      >> Sequence.Array.trimReplace Floor Empty )
         |> Sequence2D.toArray2D Tile.Empty
